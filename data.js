af179d50b1aa473247d103c5d5cb89e2a9773d06
nekoeta <nekoeta2021@gmail.com>
2025-07-20 21:44:54 +0900

    図鑑完成

--- data.js
+++ data.js
@@ -4,14 +4,54 @@ const items = [
     name: "フシギダネ",
     image: "images/fushigidane.jpg",
     description: "種ポケモン。草と毒タイプを持つ。",
-    type: "くさ / どく"
+    type: "くさ / どく",
+    evolvesTo: 2
   },
   {
     id: 2,
+    name: "フシギソウ",
+    image: "images/fushigisou.jpg",
+    description: "フシギダネの進化形。体が大きくなり、花が育ってきた。",
+    type: "くさ / どく",
+    evolvesTo: 3
+  },
+  {
+    id: 3,
+    name: "フシギバナ",
+    image: "images/fushigibana.jpg",
+    description: "最終進化形。花が大きく咲き誇る。",
+    type: "くさ / どく",
+    evolvesTo: null
+  },
+  {
+    id: 4,
     name: "ヒトカゲ",
     image: "images/hitokage.jpg",
     description: "トカゲポケモン。炎タイプ。",
-    type: "ほのお"
+    type: "ほのお",
+    evolvesTo: 5
+  },
+  {
+    id: 5,
+    name: "リザード",
+    image: "images/lizard.jpg",
+    description: "ヒトカゲの進化形。火力が増した。",
+    type: "ほのお",
+    evolvesTo: 6
+  },
+  {
+    id: 6,
+    name: "リザードン",
+    image: "images/lizardon.jpg",
+    description: "最終進化形。強力な炎を操る。",
+    type: "ほのお / ひこう",
+    evolvesTo: null
   }
-  // ... 他にも追加
 ];
+
+const evoHtml = item.evolvesTo
+  ? `<p>→ <a href="detail.html?id=${item.evolvesTo}">進化する</a></p>`
+  : `<p>最終進化形です</p>`;
+
+document.getElementById('evolution').innerHTML = evoHtml;
+
