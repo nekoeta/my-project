3ed7adc53287589ed5e86b26648ee793f933dd27
nekoeta <nekoeta2021@gmail.com>
2025-07-20 12:57:07 +0900

    図鑑

---
+++ data.js
@@ -0,0 +1,17 @@
+const items = [
+  {
+    id: 1,
+    name: "フシギダネ",
+    image: "images/fushigidane.jpg",
+    description: "種ポケモン。草と毒タイプを持つ。",
+    type: "くさ / どく"
+  },
+  {
+    id: 2,
+    name: "ヒトカゲ",
+    image: "images/hitokage.jpg",
+    description: "トカゲポケモン。炎タイプ。",
+    type: "ほのお"
+  }
+  // ... 他にも追加
+];
